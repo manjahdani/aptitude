74adf686b84e862ac5d09349dd152eeed459a8e8
manjahdani <dani.manjah@uclouvain.be>
2022-01-26 18:31:45 +0100

    SensorRole

--- launcher/src/main/sarl/be/uclouvain/launcher/UDPSender.java
+++ launcher/src/main/sarl/be/uclouvain/launcher/UDPSender.java
@@ -1,56 +1,47 @@
 package be.uclouvain.launcher;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
 
-class UDPSender
-{
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+class UDPSender {
 	private DatagramSocket client;
-	
-	public UDPSender()
-	{
-		try 
-		{
+
+	public UDPSender() {
+		try {
 			client = new DatagramSocket();
-		} 
-		catch (SocketException e) 
-		{
+		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public <T> void SendData(T data, String ip, int port)
-	{
+
+	public <T> void SendData(T data, String ip, int port) {
 		Gson jsonparser = new Gson();
-		Type dataType = new TypeToken<T>() {}.getType();
+		Type dataType = new TypeToken<T>() {
+		}.getType();
 		String msg = jsonparser.toJson(data, dataType);
-		
+
 		byte[] buffer = msg.getBytes();
-		
-		try
-		{
+
+		try {
 			InetAddress address = InetAddress.getByName(ip);
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
 			packet.setData(buffer);
-			
+
 			client.send(packet);
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void CloseUDPSender()
-	{
-		if(client != null)
-		{
+
+	public void CloseUDPSender() {
+		if (client != null) {
 			client.close();
 		}
 	}
