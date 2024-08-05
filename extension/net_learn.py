ebcf8a8fbb226fa3ecdf2b4eec4540358f834517
dani <manjahdani@gmail.com>
2024-08-05 11:49:17 +0200

    changed experiment sequence

--- extension/net_learn.py
+++ extension/net_learn.py
@@ -338,7 +338,7 @@ class Experimental_Environment:
     
     def train_and_reset(self, i, proportion, network, out_agent, n_iterations, temp_csv_path):
         # Train the agent and write to a temporary CSV file
-        network.train_new_agent(out_agent, n_iterations, proportion, temp_csv_path, name=f"cam{i+1}_and_pair_100")
+        network.train_new_agent(out_agent, n_iterations, proportion, temp_csv_path, name=f"cam{i+1}_and_helping_agents_100")
         out_agent.weights = "yolov10n"
         out_agent.flush_model()
 
@@ -349,8 +349,19 @@ class Experimental_Environment:
         #all_proportions=np.vstack((np.hstack((np.zeros((1, n_seeds-1)), np.ones((1,1)))),np.ones(n_seeds),np.array([0.1094]*5+[0.1133]*4)))
         
         #proportions for: any pair of two agents
-        all_proportions=np.hstack((np.identity(n_seeds-1), np.ones((n_seeds-1,1))))
+        #all_proportions=np.hstack((np.identity(n_seeds-1), np.ones((n_seeds-1,1))))
         
+        #proportions for: agent with any agent that benefited it when paired with it.
+        all_proportions=np.array([[1,1,0,1,0,0,0,1,1],
+                                  [1,0,0,0,0,0,0,1,1],
+                                  [1,1,0,0,0,0,0,1,1],
+                                  [0,0,0,0,0,0,0,0,1],
+                                  [1,1,1,1,0,0,0,1,1],
+                                  [0,1,0,0,0,0,1,1,1],
+                                  [1,0,0,1,1,0,1,0,1],
+                                  [1,0,1,1,0,1,1,0,1],
+                                  [1,0,1,0,0,0,0,0,1]])
+
         # Create a directory for temporary CSV files
         temp_dir = "temp_csv_files"
         os.makedirs(temp_dir, exist_ok=True)
@@ -360,12 +371,19 @@ class Experimental_Environment:
         # Create thread pool
         with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
             futures = []
-            for i in range(n_seeds):
+            #with same proportions for every seed
+            """for i in range(n_seeds):
                 for p, proportion in enumerate(all_proportions):
                     temp_csv_path = os.path.join(temp_dir, f"temp_{i}_{p}.csv")
                     temp_csv_paths.append(temp_csv_path)
-                    futures.append(executor.submit(self.train_and_reset, i, proportion, self.networks[i], self.out_agents[i], n_iterations, temp_csv_path))
+                    futures.append(executor.submit(self.train_and_reset, i, proportion, self.networks[i], self.out_agents[i], n_iterations, temp_csv_path))"""
             
+            #with 1 custom proportion per seed
+            for p, proportion in enumerate(all_proportions):
+                    temp_csv_path = os.path.join(temp_dir, f"temp_{p}.csv")
+                    temp_csv_paths.append(temp_csv_path)
+                    futures.append(executor.submit(self.train_and_reset, p, proportion, self.networks[p], self.out_agents[p], n_iterations, temp_csv_path))
+
             # Wait for all futures to complete
             concurrent.futures.wait(futures)
         
@@ -378,7 +396,7 @@ class Experimental_Environment:
         os.rmdir(temp_dir)
 
 if __name__ == '__main__':
-    freeze_support()  
+    #freeze_support()  
 
     all_weights = [BASE_MODEL]*9
     all_streams = [os.path.join(PATH_TO_DATA,f'cam{i}') for i in range(1,10)]
@@ -386,4 +404,4 @@ if __name__ == '__main__':
     all_ids = [f"cam{i}" for i in range(1,10)]
 
     the_env = Experimental_Environment(9, all_weights, all_streams, all_ids)
-    the_env.main('learning_in_pair.csv', n_iterations=10_000)
+    the_env.main('learning_with_benefiting_agents.csv', n_iterations=10_000)
